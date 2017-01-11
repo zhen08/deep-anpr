cfbca31b0f9ebd140ce93f4a731a2ab46cb9f518
zhen08 <zhen08@gmail.com>
2017-01-11 23:44:50 +1300

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -1,5 +1,7 @@
+sudo apt install unzip
 wget http://vision.princeton.edu/projects/2010/SUN/SUN397.tar.gz
 wget http://dl.dafont.com/dl/?f=uk_number_plate
+mv index.html?f=uk_number_plate uk_number_plate.zip
 unzip uk_number_plate.zip
 mkdir fonts
 cp UKNumberPlate.ttf ./fonts/
