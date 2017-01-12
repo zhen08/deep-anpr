2ad234a7d487c004034757bcbc468bea45b0fc17
zhen08 <zhen08@gmail.com>
2017-01-12 16:19:42 +1300

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -1,4 +1,7 @@
-sudo apt install unzip
+sudo apt install -y unzip
+export PATH=/usr/local/cuda/bin:$PATH
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/cuda/lib64"
+export CUDA_HOME=/usr/local/cuda
 wget http://vision.princeton.edu/projects/2010/SUN/SUN397.tar.gz
 wget http://dl.dafont.com/dl/?f=uk_number_plate
 mv index.html?f=uk_number_plate uk_number_plate.zip
